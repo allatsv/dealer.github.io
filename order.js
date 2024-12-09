4140e2199286aba062c900484e34d1ba0db61da5
allatsv <cvetkova20022@gmail.com>
2024-12-09 06:00:40 +0300

    08-0

--- order.js
+++ order.js
@@ -1,24 +1,50 @@
-let buttonCountMinus = document.querySelectorAll('.make-order__card-total-img')
-let price = document.querySelectorAll('.cost')
-let finalPrice = document.querySelector('.costRes')
-let priceF = 0
-finalPrice.innerHTML = priceF
-
-price.forEach((elem) => {
-    console.log(elem.innerHTML)
-    priceF =  0+ Number(finalPrice.innerHTML)+ Number(elem.innerHTML)
-    finalPrice.innerHTML = priceF
-})
-
+let itemCost = 0
+let itemCost2 = 0
+let result = 0
+let cost = document.querySelectorAll('.cost')
+let costRes = document.querySelector('.costRes')
+let minus = document.querySelectorAll('.make-order__card-total-img')
+let clean = document.querySelector('.make-order__clear-text')
+let card = document.querySelectorAll('.make-order__card')
+let quantity = document.querySelector('.quantity')
+let counterProduct = 0
 
 
-function resetPrice(){
-  buttonCountMinus.forEach((elem) => {
-    elem.addEventListener('click',function(){
-        finalPrice.innerHTML = priceF - 21999
+
+
+clean.addEventListener('click',function(){
+    card.forEach(elem=>{
+      elem.remove()
+      costRes.innerHTML = 0
+      quantity.innerHTML = 0
     })
-  })
+
+})
+function counter(){
+  cost.forEach(elem=>{
+    counterProduct++
+    quantity.innerHTML = counterProduct
+    itemCost = Number(elem.textContent)
+    result = result + itemCost
+    costRes.innerHTML = result
+    }
+    
+    )
 }
 
 
-resetPrice()
+
+minus.forEach(elem=>{
+    elem.addEventListener("click", function(){
+      itemCost2 = (elem.previousElementSibling.firstChild.innerHTML)
+      costRes.innerHTML = Number(costRes.innerHTML) - Number(itemCost2)
+      
+      elem.closest(".make-order__card-flex").remove()
+      counterProduct = quantity.innerHTML-1
+      quantity.innerHTML = counterProduct
+    }
+  )
+  }
+  
+  )
+
